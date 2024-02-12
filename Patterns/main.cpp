6f50e61f65fe611fae14dc863b8c6d83c3834aca
chandangepala <chandangepala@gmail.com>
2024-02-13 00:09:41 +0530

    Initial Commit

---
+++ Patterns/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Patterns
+//
+//  Created by Chandan Gepala on 13/02/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
